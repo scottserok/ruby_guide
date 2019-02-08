bea0517df236827fc891711690d44f3c0ea8782e
Scott Serok <167059+scottserok@users.noreply.github.com>
2019-02-08 02:28:58 -0500

    add comments to widget_factory sinatra DSL methods

--- widget_factory/lib/widget_factory.rb
+++ widget_factory/lib/widget_factory.rb
@@ -5,14 +5,16 @@ require './lib/widget_factory/database'
 
 ENV['RACK_ENV'] ||= 'development'
 
-# Show all of our widgets
+# Called when the web server receives HTTP GET /
+# Retrieves all of the widgets in the database and renders lib/views/index.erb
 get '/' do
   db = WidgetFactory::Database.new
   @widgets = db.all
   erb :index
 end
 
-# Create a new widget and send the user back to the home page
+# Called when the web server receives HTTP POST /
+# Creates a new widget and send the user back to root path
 post '/' do
   db = WidgetFactory::Database.new
   db.create name: params[:name], color: params[:color]
